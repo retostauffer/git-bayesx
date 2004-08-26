9a902bf083935892d0cd0bfef592d6b3cac213cc
lang <lang>
2004-08-26 12:47:50 +0000

    no message

---
+++ internet/bayesx/chmod.bat
@@ -0,0 +1,67 @@
+chmod o+rx bayesx.html
+chmod o+rx bayesxwhat.html
+chmod o+rx bayesxmaps.html
+chmod o+rx bayesxtutorials.html
+chmod o+rx bayesxexamples.html
+chmod o+rx bayesxdownload.html
+chmod o+rx bayesxnews.html
+chmod o+rx bayesxbugs.html
+chmod o+rx rents.html
+chmod o+rx unemployment.html
+
+chmod o+rx bayesicon.gif
+chmod o+rx bjahr.gif
+chmod o+rx mage2.gif
+chmod o+rx mdate2.gif
+chmod o+rx mrand2c.gif
+chmod o+rx msea2.gif
+chmod o+rx mspat2c.gif
+chmod o+rx mtime2.gif
+chmod o+rx muenchenres.gif
+chmod o+rx viertel_onord.gif
+chmod o+rx wfl.gif
+
+chmod o+rx belgium.gra
+chmod o+rx germany1993.gra
+chmod o+rx germany1993_2.gra
+chmod o+rx germany2001.gra
+chmod o+rx germany2001_2.gra
+chmod o+rx germany9301.gra
+chmod o+rx germany9301_2.gra
+chmod o+rx india_dist.gra
+chmod o+rx india_states.gra
+chmod o+rx meckpomm.gra
+chmod o+rx munich_bezt.gra
+chmod o+rx munich_bezv.gra
+chmod o+rx nigeria.gra
+chmod o+rx tanzania.gra
+chmod o+rx westerngermany.gra
+chmod o+rx zambia.gra
+
+chmod o+rx belgium.bnd
+chmod o+rx germany1993.bnd
+chmod o+rx germany1993_2.bnd
+chmod o+rx germany2001.bnd
+chmod o+rx germany2001_2.bnd
+chmod o+rx germany9301.bnd
+chmod o+rx germany9301_2.bnd
+chmod o+rx india_dist.bnd
+chmod o+rx india_states.bnd
+chmod o+rx meckpomm.bnd
+chmod o+rx munich_bezt.bnd
+chmod o+rx munich_bezv.bnd
+chmod o+rx nigeria.bnd
+chmod o+rx tanzania.bnd
+chmod o+rx westerngermany.bnd
+chmod o+rx zambia.bnd
+
+chmod o+rx nigeria.jpg
+chmod o+rx tanzania.jpg
+chmod o+rx zambia.jpg
+
+chmod o+rx remltutorial.zip
+chmod o+rx remltutorial.prg
+chmod o+rx mcmctutorial.zip
+chmod o+rx mcmctutorial.prg
+chmod o+rx zambia.raw
+
