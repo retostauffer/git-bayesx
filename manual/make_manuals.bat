116d8a9fc246c37b79c4ec719c904eb500b2867e
kneib <kneib>
2005-03-07 16:32:06 +0000

    no message

---
+++ manual/make_manuals.bat
@@ -0,0 +1,30 @@
+latex manual
+latex manual_star
+latex manual_tutorials
+
+latex manual
+latex manual
+latex manual_star
+latex manual_star
+latex manual_tutorials
+latex manual_tutorials
+
+makeindex manual
+makeindex manual_star
+makeindex manual_tutorials
+
+latex manual
+latex manual_star
+latex manual_tutorials
+
+dvips manual
+dvips manual_star
+dvips manual_tutorials
+
+ps2pdf manual.ps
+ps2pdf manual_star.ps
+ps2pdf manual_tutorials.ps
+
+cp manual.pdf ..\internet\bayesx\manual\reference_manual.pdf
+cp manual_star.pdf ..\internet\bayesx\manual\methodology_manual.pdf
+cp manual_tutorials.pdf ..\internet\bayesx\manual\tutorials_manual.pdf
