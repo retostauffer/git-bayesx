9363719d227400e01db273a79726a6666218d6d2
andib <andib>
2005-05-20 09:29:04 +0000

    no message

--- manual/make_manuals.bat
+++ manual/make_manuals.bat
@@ -25,10 +25,10 @@ ps2pdf manual.ps
 ps2pdf manual_star.ps
 ps2pdf manual_tutorials.ps
 
-cp manual.pdf ..\internet\bayesx\manual\reference_manual.pdf
-cp manual_star.pdf ..\internet\bayesx\manual\methodology_manual.pdf
-cp manual_tutorials.pdf ..\internet\bayesx\manual\tutorials_manual.pdf
+copy manual.pdf ..\internet\bayesx\manual\reference_manual.pdf
+copy manual_star.pdf ..\internet\bayesx\manual\methodology_manual.pdf
+copy manual_tutorials.pdf ..\internet\bayesx\manual\tutorials_manual.pdf
 
-cp manual.pdf ..\java\doc\reference_manual.pdf
-cp manual_star.pdf ..\java\doc\methodology_manual.pdf
-cp manual_tutorials.pdf ..\java\doc\tutorials_manual.pdf
+copy manual.pdf ..\java\doc\reference_manual.pdf
+copy manual_star.pdf ..\java\doc\methodology_manual.pdf
+copy manual_tutorials.pdf ..\java\doc\tutorials_manual.pdf
