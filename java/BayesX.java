f91bf69fe2e41f3d024b6d3acd944b9735e93bdd
kneib <kneib>
2005-07-04 12:07:45 +0000

    no message

--- java/BayesX.java
+++ java/BayesX.java
@@ -526,7 +526,7 @@ public BayesX()
 	jDesktopPanel.add(output);
 	output.setBounds(registryArray[2],registryArray[3],registryArray[4],registryArray[5]);
 	Out("BayesX - Software for Bayesian inference\n\n",true,false,(short)11,0,0,0);
-	Out("Version 1.30 (08.11.2004)\n\n");
+	Out("Version 1.40 (06.07.2005)\n\n");
 
 //	Out("Note: When running time consuming computations it is useful to reduce the priority of BayesX in the Windows Task-Manager!\n\n");
 	hasBeenSaved = false;
@@ -1129,7 +1129,7 @@ public void actionPerformed(ActionEvent ae)
         else if(source.equals("About BayesX"))
                 {
 		JOptionPane.showMessageDialog(this,"BayesX\n\nSoftware for Bayesian Inference\n"+
-			"Version 1.30 (08.11.2004)\n\n"+
+			"Version 1.40 (06.07.2005)\n\n"+
 			"developed at\n"+
 			"  University of Munich\n"+
 			"  Department of Statistics\n"+
@@ -3083,7 +3083,7 @@ public static void main(String[] args)
 		{
 		BayesX b = new BayesX();
 		b.consoleInput = true;
-		System.out.println("\nBayesX Version 1.3");
+		System.out.println("\nBayesX Version 1.40");
 		System.out.println("Software for Bayesian Inference");
 		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 		String str = null;
