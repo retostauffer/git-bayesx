24764a51f60c7e2749efea432be8d7364752fe4d
lang <lang>
2006-09-26 19:43:56 +0000

    no message

--- mcmc/distribution.cpp
+++ mcmc/distribution.cpp
@@ -4473,7 +4473,7 @@ DISTRIBUTION_vargaussian::DISTRIBUTION_vargaussian(const double & scale_initial,
   {
 
   family = "VarianceGaussian";
-  usegamma=false;
+  usegamma=true;
 
   if (usegamma==false)
     scaleexisting=false;
@@ -4494,7 +4494,7 @@ DISTRIBUTION_vargaussian::DISTRIBUTION_vargaussian(
   {
 
   family = "VarianceGaussian";
-  usegamma=false;
+  usegamma=true;
 
   if (usegamma==false)
     scaleexisting=false;
@@ -4515,7 +4515,7 @@ DISTRIBUTION_vargaussian::DISTRIBUTION_vargaussian(
   {
 
   family = "VarianceGaussian";
-  usegamma=false;
+  usegamma=true;
 
   if (usegamma==false)
     scaleexisting=false;
