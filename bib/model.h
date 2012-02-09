6740e378ef39e1407465b2121ce204a455bb2dae
lang <lang>
2012-02-09 10:37:47 +0000

    no message

--- bib/model.h
+++ bib/model.h
@@ -17,8 +17,10 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
 
-
+#if defined(BORLAND_OUTPUT_WINDOW)
+#elif
 using namespace std;
+#endif
 
 #if !defined (MODEL_INCLUDED)
 #define MODEL_INCLUDED
