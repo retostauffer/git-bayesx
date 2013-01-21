fd3b3a000bdafc14c4cd10daaf8d06198f429b83
dadler <dadler>
2013-01-21 14:08:59 +0000

    use export_type.h

--- java/BayesXdll.h
+++ java/BayesXdll.h
@@ -1,9 +1,1 @@
-
-#if defined (__BUILDING_THE_DLL)
-#define __EXPORT_TYPE __export
-#elif defined (__BUILDING_GNU)
-#define __EXPORT_TYPE __declspec(dllexport)
-#else
-#define __EXPORT_TYPE __import
-#endif
-
+#include "../export_type.h"
