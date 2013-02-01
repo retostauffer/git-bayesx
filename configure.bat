6f648e099e5e77d2ff77b392b6afc7cfaac3c8ef
tkneib <tkneib>
2013-02-01 10:49:34 +0000

    adapted configure.bat to work on 64bit windows deleted gcc.bat to avoid confusion with gcc.exe

--- configure.bat
+++ configure.bat
@@ -1,11 +1,39 @@
-# 64 bit Windows (Thomas' office PC)
-# add mingw to the path
-# add cmake to the path
-cmake -G "MinGW Makefiles" -D CMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -D CMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ .
+######################################
+# 64 bit Windows (Thomas' office PC) #
+######################################
 
-# 32 bit Windows (Thomas' Notebook)
-# add mingw to the path
-# add cmake to the path
+# add r-tools mingw to the path to enable access to the compiler 
+c:\env\mingw-rtools.bat
+# add codeblocks mingw to the path to enable access to mingw32-make 
+c:\env\mingw-cb.bat
+# add cmake to the path 
+c:\env\cmake.bat
+# add java to the path
+c:\env\java.bat
+
+# call cmake with explicit compiler specification for 64bit comilation
+# if there has been no previous call that set the compiler:
+cmake -G "MinGW Makefiles" -D CMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -D CMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ .
+#otherwise
+cmake -G "MinGW Makefiles" .
+
+# call make (with no. of kernels defined by -j)
+mingw32-make -j4
+
+# compile java version
+cd java
+javac BayesX.java
+java BayesX
+
+
+#####################################
+# 32 bit Windows (Thomas' Notebook) #
+#####################################
+
+# add mingw to the path 
+c:\env\mingw-cb.bat
+# add cmake to the path 
+c:\env\cmake.bat
 # if there has been no previous call that set the compiler:
 cmake -G "MinGW Makefiles" -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++ .
 # otherwise:
