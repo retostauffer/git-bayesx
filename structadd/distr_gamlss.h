f077ef7f4d5fa39635a848214c2bdc351921b899
nklein <nklein>
2014-10-27 13:50:44 +0000

    changes JanF

--- structadd/distr_gamlss.h
+++ structadd/distr_gamlss.h
@@ -32,7 +32,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
 #if defined (__BUILDING_LINUX)
   #include "gsl/gsl_randist.h"
 #else
-  #include "gsl\gsl_randist.h"
+  #include "gsl/gsl_randist.h"
 #endif
 
 namespace MCMC
